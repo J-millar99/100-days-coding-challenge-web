552efde1df01d0728f6311b955c7a68061e2ae10
J-millar99 <wlwogus33@gmail.com>
2026-03-20 03:17:35 +0900

    Tic, Tac, Toe Project

---
+++ scripts/config.js
@@ -0,0 +1,38 @@
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid; // +'1' => 1
+    playerConfigOverlayElement.style.display = 'block';
+    backdropElement.style.display = 'block';
+}
+
+function closePlayerConfig() {
+    playerConfigOverlayElement.style.display = 'none';
+    backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent = '';
+    formElement.firstElementChild.lastElementChild.value = ''
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault(); // 화면 전송 차단
+    console.log(event.target);
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername').trim();
+
+    if (!enteredPlayername) { // enteredPlayername === ''
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputElement.textContent = 'Please enter a valid name!';
+        return;
+    }
+
+    const updatedPlayerDatyElement = document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDatyElement.children[1].textContent = enteredPlayername; // h3
+
+    // if (editedPlayer === 1) {
+    //     players[0].name = enteredPlayername;
+    // } else {
+    //     players[1].name = enteredPlayername;
+    // }
+
+    players[editedPlayer - 1].name = enteredPlayername;
+    closePlayerConfig();
+}
